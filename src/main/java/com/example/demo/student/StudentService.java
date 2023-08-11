5cdf6e45482a8ff713953bee49b846790d80c373
Raiyan Shaheen <raiyanshaheen@Raiyans-MacBook-Pro-2.local>
2023-08-11 13:02:53 +0100

    start of tutorial files

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "rai",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21,
+                        "rai@example.com"
+
+                )
+        );
+    }
+}
